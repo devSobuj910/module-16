e76561ad078e36910148869fd80c620d0f9cf2bd
devSobuj910 <ftsobuj222@gmail.com>
2022-08-09 03:13:08 +0600

    uptdet var name

--- problem solving/problem2.js
+++ problem solving/problem2.js
@@ -1,18 +1,17 @@
-var math=90;
-var biology=44;
-var chm=98;
-var bangla =56;
-var english=54;
+var math=75.25;
+var biology=65;
+var chm=80;
+var bangla =35.45;
+var english=99.50;
 
 var totaleMark=math + biology+ chm + bangla + english;
 
-console.log(totaleMark)
+// console.log(totaleMark)
 
 
 
 var totalSbj=5;
-var avaregeMark=totaleMark / 5;
-
-console.log(avaregeMark)
-
+var avaregeMark=totaleMark / totalSbj;
+var fixNum =avaregeMark.toFixed(2)
+console.log(fixNum);
 
