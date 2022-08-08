dab2ed06470b4ff355c3be53cb4bf1e8fc78d3f6
devSobuj910 <ftsobuj222@gmail.com>
2022-08-09 01:25:51 +0600

    first commit

---
+++ const.js
@@ -0,0 +1,10 @@
+//   coment this text defarent  var let  between constant const  const can not re  asing;
+//  let x=20;
+//  x=34;
+
+//  defarent 
+//  const y=44;
+//  y=33;
+
+// //  coment this  text  eror this re asing
+//   console.log(y)
---
+++ let.js
@@ -0,0 +1,14 @@
+// let or var  deafarent  let cannot to be  re declaered
+
+
+
+// let x=33;
+// let x=55;
+//     coment this text  you cannot declared
+// console.log(x)
+
+
+
+// var y=5;
+// var y=9;
+// console.log(y)
